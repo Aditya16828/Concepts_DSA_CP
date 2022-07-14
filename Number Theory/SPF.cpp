b581739abe94a32a914bc63887fe45361687967e
Aditya <adimukh2001@gmail.com>
2022-07-15 04:22:53 +0530

    Added files

---
+++ Number Theory/SPF.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define mod 1000000007
+#define inf (long long int)1e18
+#define log(args...)    { std::string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); std::stringstream _ss(_s); std::istream_iterator<std::string> _it(_ss); err(_it, args); }
+#define logarr(arr,a,b) for(int z=(a);z<=(b);z++) std::cout<<(arr[z])<<" ";std::cout<<std::endl;
+#define vec std::vector
+#define vll std::vector<long long int>
+#define vi std::vector<int>
+#define vb std::vector<bool>
+#define pii std::pair<int, int>
+#define pllll std::pair<long long int, long long int>
+#define loop(i, a, b) for(int i=a;i<=b;i++)
+#define rloop(i, a, b) for(int i=a;i>=b;i--)
+#define iter_all(el, cont) for(auto &el:cont)
+
+void err(std::istream_iterator<std::string> it) {}
+template<typename T, typename... Args>
+void err(std::istream_iterator<std::string> it, T a, Args... args) {
+	std::cout << *it << " = " << a << std::endl;
+	err(++it, args...);
+}
+
+void file_i_o() {
+	std::ios_base::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+
+// Time complexity of factorising a number using the pi[] is O(log(n))
+int const max_size = (int)(1e5 + 5);
+std::vector<int> pi(max_size);
+std::vector<int> primes;
+
+void spf() {
+	for (int i = 1; i < max_size; i++) pi[i] = i;
+	pi[1] = -1;
+	for (int i = 2; i < max_size; i++) {
+		if (pi[i] == i) {
+			primes.push_back(i);
+		}
+		for (int j = 0; (j < primes.size()) and (primes[j] <= pi[i]) and i * primes[j] < max_size; j++) {
+			pi[i * primes[j]] = primes[j];
+		}
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	clock_t begin = clock();
+	file_i_o();
+	// Write your code here....
+
+	spf();
+	loop(i, 2, 50) {
+		std::cout << pi[i] << ", ";
+	}
+
+#ifndef ONLINE_JUDGE
+	clock_t end = clock();
+	std::cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+	return 0;
+}
