dd41e20b396a214f6ed3133239e393f04e0d5f89
Aditya Mukherjee <mukherjee.aditya.official@gmail.com>
2022-12-24 11:07:08 +0530

    added codes

--- Graphs/FCAR_Codechef.cpp
+++ Graphs/FCAR_Codechef.cpp
@@ -1,3 +1,4 @@
+// https://www.codechef.com/problems-old/INSQ15_F
 #include <bits/stdc++.h>
 #define ll long long int
 #define mod 1000000007
@@ -31,47 +32,36 @@ void file_i_o() {
 #endif
 }
 
-std::vector<std::list<int>> g;
+std::vector<std::list<pllll>> g;
 
-struct comparator {
-    bool operator()(pllll const& a, pllll const& b) {
-        if (a.second > b.second)
-            return true;
-        else if(a.first < b.first)
-            return true;
-        return false;
+ll bfs(ll src, ll dest, ll n) {
+    std::priority_queue<pllll, std::vector<pllll>, std::greater<pllll>> pq;
+
+    pq.push({ 0, src });
+
+    std::vector<ll> minCost(n, inf);
+    minCost[src] = 0;
+
+    std::vector<bool> vis(2 * n + 2, false);
+
+    while (not pq.empty()) {
+        pllll curr = pq.top();
+        pq.pop();
+        // std::cout << "{" << curr.first << ", " << curr.second << "}, ";
+
+        if (curr.second == dest) break;
+        if (vis[curr.second]) continue;
+
+        vis[curr.second] = true;
+
+        for (auto& ne : g[curr.second]) {
+            if (minCost[ne.first] > minCost[curr.second] + ne.second) {
+                minCost[ne.first] = minCost[curr.second] + ne.second;
+                pq.push({ minCost[ne.first], ne.first });
+            }
+        }
     }
-};
-
-// ll bfs(int src, int dest, vll& cost, int n) {
-//     std::priority_queue<pllll, std::vector<pllll>, comparator> pq;
-
-//     pq.push({ src, cost[src] });
-
-//     std::vector<ll> minCost(n, INT_MAX);
-//     minCost[src] = cost[src];
-
-//     ll ans = cost[src];
-
-//     while (not pq.empty()) {
-//         pllll curr = pq.top();
-//         pq.pop();
-//         std::cout << "{" << curr.first << ", " << curr.second << "}, ";
-
-//         if (curr.first == dest) break;
-
-//         for (auto& ne : g[curr.first]) {
-//             ans += curr.second;
-//             pq.push(ne);
-//         }
-//     }
-//     // logarr(minCost, 0, n - 1);
-//     std::cout<<"\n";
-//     return ans;
-// }
-
-void bfs_up(int src, int dest){
-    
+    return minCost[dest];
 }
 
 int main(int argc, char const* argv[]) {
@@ -79,52 +69,47 @@ int main(int argc, char const* argv[]) {
     file_i_o();
     // Write your code here....
 
-    int n, r;
+    ll n, r;
     std::cin >> n >> r;
-    vll h(n, 0), c(n, 0);
-    for (int i = 0;i < n;i++) std::cin >> h[i];
-    for (int i = 0;i < n;i++) std::cin >> c[i];
+    vll h(n+1, 0), c(n+1, 0);
+    for (int i = 1;i <= n;i++) std::cin >> h[i];
+    for (int i = 1;i <= n;i++) std::cin >> c[i];
 
-    // g.resize(2 * n, std::list<pllll>());
-    g.resize(n, std::list<int>());
+    g.resize(2 * n + 2, std::list<pllll>());
 
     /*
     up -> 0 to n
-    down -> n to 2n
+    down -> n+1 to 2n+1
     */
-    // loop(_, 0, r - 1) {
-    //     int u, v;
-    //     std::cin >> u >> v;
-    //     u--, v--;
-    //     if (h[u] <= h[v]) {
-    //         g[u].push_back({ v, 0 });
-    //         // g[2*v].push_back(2*u);
-    //     } else if (h[u] >= h[v]) {
-    //         g[u + n].push_back({ v + n,0 });
-    //         // g[v].push_back(u);
-    //     }
-    // }
-    // loop(i, 0, n - 1) {
-    //     g[i].push_back({ i + n, c[i] });
-    // }
+    loop(_, 0, r - 1) {
+        ll u, v;
+        std::cin >> u >> v;
+        if (h[u] <= h[v]) {
+            g[u].push_back({ v, 0 });
+        }
+        if (h[u] >= h[v]) {
+            g[u + n].push_back({ v + n,0 });
+        }
 
-    while(r--){
-        int u, v;
-        u--, v--;
-        g[u].push_back(v);
-        g[v].push_back(u);
+        if (h[v] <= h[u]) {
+            g[v].push_back({ u, 0 });
+        }
+        if (h[v] >= h[u]) {
+            g[v + n].push_back({ u + n, 0 });
+        }
     }
+    loop(i, 1, n) {
+        g[i].push_back({ i + n, c[i] });
+        g[i + n].push_back({ i, c[i] });
+    }
+    g[0].push_back({1, c[1]});
+    g[0].push_back({n+1, c[1]});
+    g[2*n].push_back({2*n+1, 0});
+    g[n].push_back({2*n+1, 0});
 
-    // loop(i, 0, 2 * n - 1) {
-    //     std::cout << i << "->";
-    //     for (auto& el : g[i])
-    //         std::cout << "{" << el.first << ", " << el.second << "}, ";
-    //     std::cout << "\n";
-    // }
-    // std::cout << "\n\n";
 
-    ll res = bfs(0, n - 1, c, n);
-    if (res == INT_MAX) {
+    ll res = bfs(0, 2 * n+1, 2 * n + 2);
+    if (res == inf) {
         std::cout << "-1\n";
     } else {
         std::cout << res << "\n";
